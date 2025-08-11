8af55d8ce2a3dd5b06ba9cd88a9616b961ab8f60
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-08-11 16:54:11 +0800

    fix nonce

--- src/stores/api/client.ts
+++ src/stores/api/client.ts
@@ -1,4 +1,5 @@
 import { HttpError } from '@api/http-error.ts';
+import { v4 as uuidv4 } from 'uuid';
 
 const ENV_PROD = 'production';
 
@@ -6,14 +7,12 @@ export const defaultCreds: ApiClientOptions = {
 	env: import.meta.env.VITE_API_ENV as string,
 	apiKey: import.meta.env.VITE_PUBLIC_KEY as string,
 	apiUsername: import.meta.env.VITE_ACCOUNT_NAME as string,
-	apiSignature: import.meta.env.VITE_PUBLIC_SIGNATURE as string,
 };
 
 export interface ApiClientOptions {
 	env: string;
 	apiKey: string;
 	apiUsername: string;
-	apiSignature: string;
 }
 
 export interface CacheEntry<T> {
@@ -31,21 +30,17 @@ export class ApiClient {
 	private readonly apiKey: string;
 	private readonly basedURL: string;
 	private readonly apiUsername: string;
-	private readonly apiSignature: string;
-	private readonly nonce: string;
 	private readonly timestamp: string;
 
 	constructor(options: ApiClientOptions) {
 		this.env = options.env;
-		this.nonce = this.getNonce();
 		this.apiKey = options.apiKey;
 		this.apiUsername = options.apiUsername;
-		this.apiSignature = options.apiSignature;
 		this.basedURL = `${import.meta.env.VITE_API_URL}`;
 		this.timestamp = Math.floor(Date.now() / 1000).toString();
 	}
 
-	private getNonce(): string {
+	public createNonce(): string {
 		return crypto.getRandomValues(new Uint8Array(16)).reduce((s, b) => s + b.toString(16).padStart(2, '0'), '');
 	}
 
@@ -79,8 +74,7 @@ export class ApiClient {
 		const headers = new Headers();
 
 		headers.append('X-API-Key', this.apiKey);
-		headers.append('X-API-Nonce', this.nonce);
-		headers.append('X-Request-ID', this.nonce);
+		headers.append('X-Request-ID', uuidv4());
 		headers.append('User-Agent', 'oullin/web-app');
 		headers.append('X-API-Timestamp', this.timestamp);
 		headers.append('X-API-Username', this.apiUsername);
@@ -119,13 +113,28 @@ export class ApiClient {
 			.join('');
 	}
 
-	public async post<T>(url: string, data: object): Promise<T> {
+	private getSortedQuery(u: string): string {
+		const params = new URL(u).searchParams;
+
+		params.sort();
+		return params.toString();
+	}
+
+	private async getSignature(method: string, uri: URL, body: string, nonce: string): Promise<string> {
+		const content = await this.sha256Hex(body);
+
+		const canonical = [method.toUpperCase(), uri.pathname || '/', this.getSortedQuery(uri.href), this.apiUsername, this.apiKey, this.timestamp, nonce, content].join('\n');
+
+		return await this.hmacSha256Hex(this.apiKey, canonical);
+	}
+
+	public async post<T>(url: string, nonce: string, data: object): Promise<T> {
 		const headers = this.createHeaders();
 		const fullUrl = new URL(url, this.basedURL);
-
 		const content: string = JSON.stringify(data);
-		const signature = await this.getSignature('POST', fullUrl, content);
+		const signature = await this.getSignature('POST', fullUrl, content, nonce);
 
+		headers.append('X-API-Nonce', nonce);
 		headers.append('X-API-Signature', signature);
 
 		const response = await fetch(fullUrl.href, {
@@ -141,22 +150,7 @@ export class ApiClient {
 		return await response.json();
 	}
 
-	private sortedQuery(u: string): string {
-		const params = new URL(u).searchParams;
-
-		params.sort();
-		return params.toString();
-	}
-
-	private async getSignature(method: string, uri: URL, body: string): Promise<string> {
-		const content = await this.sha256Hex(body);
-
-		const canonical = [method.toUpperCase(), uri.pathname || '/', this.sortedQuery(uri.href), this.apiUsername, this.apiKey, this.timestamp, this.nonce, content].join('\n');
-
-		return await this.hmacSha256Hex(this.apiKey, canonical);
-	}
-
-	public async get<T>(url: string): Promise<T> {
+	public async get<T>(url: string, nonce: string): Promise<T> {
 		const headers = this.createHeaders();
 		const cached = this.getFromCache<T>(url);
 		const fullUrl = new URL(url, this.basedURL);
@@ -165,8 +159,9 @@ export class ApiClient {
 			headers.append('If-None-Match', cached.etag);
 		}
 
-		const signature = await this.getSignature('GET', fullUrl, '');
+		const signature = await this.getSignature('GET', fullUrl, '', nonce);
 		headers.append('X-API-Signature', signature);
+		headers.append('X-API-Nonce', nonce);
 
 		const response = await fetch(fullUrl.href, {
 			method: 'GET',
