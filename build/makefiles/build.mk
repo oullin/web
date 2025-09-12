bf8b083cfbf0e27198de5bbe6ff4b0b8d26bb779
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-09-12 12:04:48 +0800

    new target

--- build/makefiles/build.mk
+++ build/makefiles/build.mk
@@ -1,4 +1,4 @@
-.PHONY: build-ci build-deploy build-release local-fresh local-up
+.PHONY: build-ci build-deploy build-release local-fresh local-up build-remove
 
 WEB_TAG ?= web-prod-builder
 BUILD_VERSION ?= latest
@@ -16,6 +16,9 @@ build-release:
 
 build-deploy:
 	docker compose --env-file ./.env --profile prod up -d --no-build
+
+build-remove:
+	docker compose --profile prod down --volumes --rmi all --remove-orphans
 
 # ----- LOCAL -----
 
