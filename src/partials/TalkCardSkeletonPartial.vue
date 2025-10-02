d4908d9895dd13a0084a77c7c008ede29c850819
Gus <gocanto@users.noreply.github.com>
2025-10-02 17:19:48 +0800

    Add skeleton loaders for popular talks

---
+++ src/partials/TalkCardSkeletonPartial.vue
@@ -0,0 +1,13 @@
+<template>
+        <div
+                class="relative aspect-video rounded-lg overflow-hidden bg-linear-to-tr from-slate-800 to-slate-700 odd:rotate-1 even:-rotate-1 shadow-xl animate-pulse"
+        >
+                <div class="absolute inset-0 bg-slate-900/60"></div>
+                <div class="relative h-full flex flex-col justify-between p-5">
+                        <div class="h-6 w-3/4 bg-white/30 rounded"></div>
+                        <div class="flex justify-end">
+                                <div class="h-10 w-10 rounded-full bg-white/40"></div>
+                        </div>
+                </div>
+        </div>
+</template>
