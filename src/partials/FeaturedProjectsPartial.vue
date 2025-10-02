4c63ca5f4529b5fe9f488c313faa9f415dbc3688
Gus <gocanto@users.noreply.github.com>
2025-10-02 16:27:32 +0800

    Fix dark mode gradient utility in featured projects

--- src/partials/FeaturedProjectsPartial.vue
+++ src/partials/FeaturedProjectsPartial.vue
@@ -12,7 +12,7 @@
                                         v-for="project in projects"
                                         :key="project.uuid"
                                         v-lazy-link
-                                        class="rounded-lg border border-slate-200 dark:border-slate-800 dark:bg-linear-to-t dark:from-slate-800 dark:to-slate-800/30 odd:-rotate-1 even:rotate-1 hover:rotate-0 transition-transform duration-700 hover:duration-100 ease-in-out p-5"
+                                        class="rounded-lg border border-slate-200 dark:border-slate-800 dark:bg-gradient-to-t dark:from-slate-800 dark:to-slate-800/30 odd:-rotate-1 even:rotate-1 hover:rotate-0 transition-transform duration-700 hover:duration-100 ease-in-out p-5"
                                         :href="project.url"
                                         target="_blank"
                                         rel="noopener noreferrer"
