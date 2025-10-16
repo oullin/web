4c491c282a8f43fda41cfab67f656b3765e59fd2
Gus <gocanto@users.noreply.github.com>
2025-10-16 11:30:50 +0800

    Add social links beside header search bar

--- src/partials/HeaderPartial.vue
+++ src/partials/HeaderPartial.vue
@@ -25,6 +25,28 @@
 					</div>
 				</form>
 
+				<!-- Social links -->
+				<div class="flex items-center space-x-1">
+					<a class="relative cursor-pointer p-2" href="https://github.com/gocanto" target="_blank" rel="noopener noreferrer">
+						<svg class="w-4 h-4" viewBox="0 0 16 16" aria-hidden="true">
+							<path
+								class="fill-fuchsia-400 hover:fill-fuchsia-600 dark:fill-teal-500 dark:hover:fill-teal-300"
+								d="M8 0a8 8 0 00-2.53 15.6c.4.07.55-.17.55-.38v-1.33c-2.24.49-2.71-1.08-2.71-1.08-.36-.92-.88-1.16-.88-1.16-.72-.5.05-.49.05-.49.8.06 1.22.83 1.22.83.71 1.22 1.86.87 2.31.67.07-.52.28-.87.5-1.07-1.79-.2-3.68-.9-3.68-4a3.13 3.13 0 01.83-2.16 2.92 2.92 0 01.08-2.13s.67-.22 2.2.82a7.65 7.65 0 012 0c1.53-1 2.2-.82 2.2-.82a2.92 2.92 0 01.08 2.13 3.13 3.13 0 01.83 2.16c0 3.11-1.9 3.8-3.7 4 .29.25.54.74.54 1.5v2.22c0 .21.15.45.56.38A8 8 0 008 0z"
+							/>
+						</svg>
+						<span class="sr-only">Visit GitHub profile</span>
+					</a>
+					<a class="relative cursor-pointer p-2" href="https://www.linkedin.com/in/gocanto" target="_blank" rel="noopener noreferrer">
+						<svg class="w-4 h-4" viewBox="0 0 16 16" aria-hidden="true">
+							<path
+								class="fill-fuchsia-400 hover:fill-fuchsia-600 dark:fill-teal-500 dark:hover:fill-teal-300"
+								d="M14.23 0H1.77A1.77 1.77 0 000 1.77v12.46A1.77 1.77 0 001.77 16h12.46A1.77 1.77 0 0016 14.23V1.77A1.77 1.77 0 0014.23 0zM4.95 13.33H2.67V6h2.28zM3.81 4.94a1.32 1.32 0 111.32-1.32 1.32 1.32 0 01-1.32 1.32zM13.33 13.33h-2.28v-3.9c0-.93-.02-2.13-1.3-2.13s-1.5 1.02-1.5 2.07v3.96H6a.05.05 0 01-.05-.05V6h2.19v1.01h.03a2.4 2.4 0 012.16-1.19c2.31 0 2.74 1.52 2.74 3.49z"
+							/>
+						</svg>
+						<span class="sr-only">Visit LinkedIn profile</span>
+					</a>
+				</div>
+
 				<!-- Light switch -->
 				<div class="flex flex-col justify-center">
 					<input id="light-switch" type="checkbox" name="light-switch" class="light-switch sr-only" @click="toggleDarkMode" />
