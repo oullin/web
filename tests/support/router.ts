2e14d951b85f0583498e72dfad42b07ea28d146d
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-11-18 10:02:19 +0800

    format

--- tests/support/router.ts
+++ tests/support/router.ts
@@ -2,11 +2,8 @@ import { createRouter, createMemoryHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
 
 export const createTestRouter = (routes: RouteRecordRaw[] = []) => {
-  return createRouter({
-    history: createMemoryHistory(),
-    routes: [
-      { path: '/', name: 'Home', component: { template: '<div>Home</div>' } },
-      ...routes,
-    ],
-  });
+	return createRouter({
+		history: createMemoryHistory(),
+		routes: [{ path: '/', name: 'Home', component: { template: '<div>Home</div>' } }, ...routes],
+	});
 };
