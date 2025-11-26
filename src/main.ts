d7f135373bb8dad41b1d09782fca3da0567aaa98
Gus <gocanto@users.noreply.github.com>
2025-11-26 11:37:39 +0800

    Add Sentry monitoring

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import '@css/style.css';
 import App from '@/App.vue';
 import router from '@/router';
 import { lazyLinkDirective } from '@/support/lazy-loading.ts';
+import { initializeSentry } from '@/support/sentry.ts';
 
 const app: VueApp = createApp(App);
 const pinia: Pinia = createPinia();
@@ -13,5 +14,7 @@ app.use(router);
 app.use(pinia);
 
 app.directive('lazy-link', lazyLinkDirective);
+
+initializeSentry(app, router);
 
 app.mount('#app');
